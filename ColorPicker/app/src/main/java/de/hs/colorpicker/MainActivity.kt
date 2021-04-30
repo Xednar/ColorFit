7d595a521d9e4cd1383fed8f74615482ee23c6d3
Xednar <xednar@gmail.com>
2021-04-30 23:35:50 +0200

    get api data

--- ColorPicker/app/src/main/java/de/hs/colorpicker/MainActivity.kt
+++ ColorPicker/app/src/main/java/de/hs/colorpicker/MainActivity.kt
@@ -1,49 +1,29 @@
 package de.hs.colorpicker
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.Manifest
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import java.util.concurrent.Executors
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.android.volley.AuthFailureError
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
 import java.nio.ByteBuffer
-import java.text.SimpleDateFormat
-import java.util.*
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-//typealias LumaListener = (luma: Double) -> Unit
+
 typealias ColorListener = (color: String) -> Unit
 
 class MainActivity : AppCompatActivity() {
-    /*private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
-
-        private fun ByteBuffer.toByteArray(): ByteArray {
-            rewind()    // Rewind the buffer to zero
-            val data = ByteArray(remaining())
-            get(data)   // Copy the buffer into a byte array
-            return data // Return the byte array
-        }
-
-        override fun analyze(image: ImageProxy) {
-
-            val buffer = image.planes[0].buffer
-            val data = buffer.toByteArray()
-            val pixels = data.map { it.toInt() and 0xFF }
-            val luma = pixels.average()
-
-            listener(luma)
-
-            image.close()
-        }
-    }*/
 
     class ColorAnalyzer(private val listener: ColorListener) : ImageAnalysis.Analyzer {
 
@@ -87,22 +67,26 @@ class MainActivity : AppCompatActivity() {
             val g = y - (0.698001 * v) - (0.337633 * u)
             val b = y + (1.732446 * u)
 
-            return Triple(r,g,b)
+            return Triple(Math.abs(r), Math.abs(g), Math.abs(b))
         }
 
 
         // analyze the color
         override fun analyze(image: ImageProxy) {
             val colors = getRGBfromYUV(image)
-            var hexColor = String.format("%d-%d-%d", colors.first.toInt(), colors.second.toInt(), colors.third.toInt())
+            val hexColor = String.format("%d-%d-%d", colors.first.toInt(), colors.second.toInt(), colors.third.toInt())
             listener(hexColor)
             image.close()
         }
     }
 
     private var imageCapture: ImageCapture? = null
+    private var currentColor = "0-0-0"
+    private var firstColor: String? = null
+    private var colorResult: String? = null
+    private var recommendedColors: List<String>? = null
+    private var distance: Double? = null
 
-    private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -118,11 +102,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Set up the listener for take photo button
-        camera_capture_button.setOnClickListener { takePhoto() }
-
-        outputDirectory = getOutputDirectory()
+        camera_capture_button.setOnClickListener {
+            if (recommendedColors == null) {
+            takeFirstColor()
+        } }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
+
     }
 
     override fun onRequestPermissionsResult(
@@ -140,34 +126,58 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
+    private fun takeFirstColor() {
+        if(imageCapture == null) return
 
-        // Create time-stamped output file to hold the image
-        val photoFile = File(
-                outputDirectory,
-                SimpleDateFormat(FILENAME_FORMAT, Locale.US
-                ).format(System.currentTimeMillis()) + ".jpg")
+        firstColor = currentColor
+        val rgbColors = firstColor!!.split("-")
 
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        // Set up image capture listener, which is triggered after photo has
-        // been taken
-        imageCapture.takePicture(
-                outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
-            override fun onError(exc: ImageCaptureException) {
-                Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+        val queue = Volley.newRequestQueue(this)
+       // val url = "http://thecolorapi.com/scheme?rgb=${rgbColors[0]},${rgbColors[1]},${rgbColors[2]}&format=json&mode=analogic&count=1"
+        val url = "http://colormind.io/api/"
+        val postRequest: StringRequest = object : StringRequest(Method.POST, url,
+                Response.Listener {
+                    response -> Log.d("Response", response)
+                    handleResponse(response) },
+                Response.ErrorListener {
+                    error -> handleResponse("result={[[${rgbColors[0]},${rgbColors[1]},${rgbColors[2]}]]}")
+                    Log.d("ERROR", "error => $error") }
+        ) {
+            // this is the relevant method
+            @Throws(AuthFailureError::class)
+            override fun getBody(): ByteArray {
+                val httpPostBody = "{\"input\":[[${rgbColors[0]}, ${rgbColors[1]}, ${rgbColors[2]}]],\"model\":\"default\"}"
+                return httpPostBody.toByteArray()
             }
+        }
+        queue.add(postRequest)
+    }
 
-            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                val savedUri = Uri.fromFile(photoFile)
-                val msg = "Photo capture succeeded: $savedUri"
-                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                Log.d(TAG, msg)
+    private fun handleResponse(response: String) {
+        colorResult = response
+        val colors = response.substring(11, response.length-4).replace("[", "")
+        recommendedColors = (colors.split("],"))
+        Toast.makeText(this, response, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun calculateColorDistance(r1:String, g1:String, b1:String, r2:String, g2:String, b2:String): Double {
+        return Math.sqrt(Math.pow(r1.toDouble() - r2.toDouble(), 2.0)
+                + Math.pow(g1.toDouble() - g2.toDouble(), 2.0)
+                + Math.pow(b1.toDouble() - b2.toDouble(), 2.0))
+    }
+
+    private fun calculateMinColorDistance(): Double {
+        val rgbColors1 = firstColor!!.split("-")
+        val rgbColors2 = currentColor.split("-")
+        var distance = calculateColorDistance(rgbColors1[0], rgbColors1[1], rgbColors1[2], rgbColors2[0], rgbColors2[1], rgbColors2[2])
+        for (recommended: String in recommendedColors!!) {
+            val color = recommended.split(",")
+            val dist =  calculateColorDistance(color[0], color[1], color[2], rgbColors2[0], rgbColors2[1], rgbColors2[2])
+            if (dist < distance) {
+                distance = dist
             }
-        })
+        }
+        return distance
     }
 
     private fun startCamera() {
@@ -187,19 +197,17 @@ class MainActivity : AppCompatActivity() {
             imageCapture = ImageCapture.Builder()
                     .build()
 
-           /* val imageAnalyzer = ImageAnalysis.Builder()
-                    .build()
-                    .also {
-                        it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
-                            Log.d(TAG, "Average luminosity: $luma")
-                        })
-                    }*/
-
             val imageAnalyzer = ImageAnalysis.Builder()
                     .build()
                     .also {
-                        it.setAnalyzer(cameraExecutor, ColorAnalyzer{rgb ->
-                            Log.d(TAG, "RGB: $rgb")
+                        it.setAnalyzer(cameraExecutor, ColorAnalyzer { rgb ->
+                            if (recommendedColors == null){
+                                Log.d(TAG, "RGB: $rgb")
+                            } else {
+                                distance = calculateMinColorDistance()
+                                Log.d(TAG, "First: ${firstColor}, RGB: $rgb, Dist: $distance")
+                            }
+                            currentColor = rgb;
                         })
                     }
 
@@ -214,23 +222,31 @@ class MainActivity : AppCompatActivity() {
                 cameraProvider.bindToLifecycle(
                         this, cameraSelector, preview, imageCapture, imageAnalyzer)
 
-            } catch(exc: Exception) {
+            } catch (exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
 
         }, ContextCompat.getMainExecutor(this))
+
+        var checkDistance = Runnable {
+            var text = findViewById<TextView>(R.id.camera_capture_button)
+            while (true) {
+                if (distance != null && distance!! < 20) {
+                    runOnUiThread(Runnable {
+                        text.text = ("Matching Color $currentColor")
+                    }
+                    )
+                    break
+                }
+            }
+        }
+        var checkDistanceThread = Thread(checkDistance).start()
+
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
                 baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun getOutputDirectory(): File {
-        val mediaDir = externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else filesDir
     }
 
     override fun onDestroy() {
@@ -240,8 +256,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "CameraXBasic"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA, Manifest.permission.INTERNET)
     }
 }
