124f6611002ddbcc1375153f8bf8d643bfcbba52
Xednar <xednar@gmail.com>
2021-05-21 01:10:50 +0200

    toggle torch

--- ColorPicker/app/src/main/java/de/hs/colorpicker/MainActivity.kt
+++ ColorPicker/app/src/main/java/de/hs/colorpicker/MainActivity.kt
@@ -1,22 +1,21 @@
 package de.hs.colorpicker
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Color
-import android.hardware.camera2.CameraAccessException
-import android.hardware.camera2.CameraManager
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
-import androidx.camera.core.ImageCapture.FLASH_MODE_ON
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.material.math.MathUtils
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -27,54 +26,113 @@ typealias ColorListener = (color: String) -> Unit
 class MainActivity : AppCompatActivity() {
 
     private var currentState = States.START
+    private var torchActive = true
 
     private var imageCapture: ImageCapture? = null
     private var currentColor = "0-0-0"
     private var firstColor: String? = null
     private var recommendedColors: MutableList<String>? = null
-    private var distance: Double? = null
+    private var distance = 9999.0f
+
+    private var color1Name = ""
+    private var color2Name = ""
+    private var color3Name = ""
 
     private lateinit var cameraExecutor: ExecutorService
     private lateinit var camera: Camera
+
+    private lateinit var successSound: MediaPlayer
+    private lateinit var failureSound: MediaPlayer
+    private lateinit var colorName1TextView: TextView
+    private lateinit var colorName2TextView: TextView
+    private lateinit var colorName3TextView: TextView
+    private lateinit var flashButton: ImageButton
+    private lateinit var cameraButton: Button
+
+    private var threadsRunning = false
+
+    val updateUI = Runnable {
+        runOnUiThread(Runnable {
+            colorName1TextView.setText(color1Name)
+            colorName2TextView.setText(color2Name)
+            colorName3TextView.setText(color3Name)
+            cameraButton.isEnabled = color3Name.isBlank()
+        })
+    }
+
+    val checkDistance = Runnable {
+        while (threadsRunning) {
+            if (currentState != States.SCAN) continue
+
+            val successVolume = MathUtils.lerp(1.0f, 0.0f, 20.0f / Math.max(distance, 0.0000001f))
+            val failureVolume = Math.max(0.0f, 0.8f - successVolume)
+
+            successSound.setVolume(successVolume, successVolume)
+            failureSound.setVolume(failureVolume, failureVolume)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Request camera permissions
         if (allPermissionsGranted()) {
-            //cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
             startCamera()
         } else {
-            ActivityCompat.requestPermissions(
-                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        // Set up the listener for take photo button
         camera_capture_button.setOnClickListener {
-            if (currentState == States.START) {
-                takeFirstColor()
+           var canTakeColor = true
+            if (color1Name.isBlank()) {
+                color1Name = currentColor
+            } else if (color2Name.isBlank()) {
+                color2Name = currentColor
+            } else if (color3Name.isBlank()) {
+                color3Name = currentColor
             } else {
-                Toast.makeText(this, "TODO: Add $currentColor", Toast.LENGTH_SHORT).show()
+                canTakeColor = false
+            }
+            if (canTakeColor) {
+                Thread(updateUI).start()
+                takeColor()
             }
         }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    override fun onRequestPermissionsResult(
-            requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
+    override fun onStart() {
+        super.onStart()
+        successSound = MediaPlayer.create(applicationContext, R.raw.sound_success)
+        successSound.isLooping = true
+        successSound.setOnPreparedListener(MediaPlayer.OnPreparedListener { mediaPlayer ->  mediaPlayer.start(); mediaPlayer.setVolume(0.0f, 0.0f)})
+        failureSound = MediaPlayer.create(applicationContext, R.raw.sound_failure)
+        failureSound.isLooping = true
+        failureSound.setOnPreparedListener(MediaPlayer.OnPreparedListener { mediaPlayer ->  mediaPlayer.start(); mediaPlayer.setVolume(0.0f, 0.0f)})
+
+        colorName1TextView = findViewById<TextView>(R.id.ColorName1)
+        colorName2TextView = findViewById<TextView>(R.id.ColorName2)
+        colorName3TextView = findViewById<TextView>(R.id.ColorName3)
+        flashButton = findViewById<ImageButton>(R.id.imageButton3)
+        flashButton.setOnClickListener{
+            torchActive = !torchActive
+            if (torchActive) {
+                flashButton.setImageResource(R.drawable.ic_baseline_flash_on_24)
             } else {
-                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
-                finish()
+                flashButton.setImageResource(R.drawable.ic_baseline_flash_off_24)
+            }
+            if ( camera.getCameraInfo().hasFlashUnit() ) {
+                camera.getCameraControl().enableTorch(torchActive)
             }
         }
+        cameraButton = findViewById<Button>(R.id.camera_capture_button)
+        threadsRunning = true;
+        Thread(checkDistance).start()
+        Thread(updateUI).start()
     }
 
-    private fun takeFirstColor() {
+    private fun takeColor() {
         if(imageCapture == null) return
 
         firstColor = currentColor
@@ -122,17 +180,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    private fun calculateColorDistance(r1:String, g1:String, b1:String, r2:String, g2:String, b2:String): Double {
+    private fun calculateColorDistance(r1:String, g1:String, b1:String, r2:String, g2:String, b2:String): Float {
         return Math.sqrt(Math.pow(r2.toDouble() - r1.toDouble(), 2.0)
                 + Math.pow(g2.toDouble() - g1.toDouble(), 2.0)
-                + Math.pow(b2.toDouble() - b1.toDouble(), 2.0))
+                + Math.pow(b2.toDouble() - b1.toDouble(), 2.0)).toFloat()
     }
 
-    private fun calculateMinColorDistance(): Double {
+    private fun calculateMinColorDistance(): Float {
        // val rgbColors1 = firstColor!!.split("-")
         val rgbColors2 = currentColor.split("-")
-        var distance = 999.0//calculateColorDistance(rgbColors1[0], rgbColors1[1], rgbColors1[2], rgbColors2[0], rgbColors2[1], rgbColors2[2])
+        var distance = 999.0f//calculateColorDistance(rgbColors1[0], rgbColors1[1], rgbColors1[2], rgbColors2[0], rgbColors2[1], rgbColors2[2])
         for (recommended: String in recommendedColors!!) {
             val color = recommended.split(",")
             val dist =  calculateColorDistance(color[0], color[1], color[2], rgbColors2[0], rgbColors2[1], rgbColors2[2])
@@ -148,10 +205,8 @@ class MainActivity : AppCompatActivity() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener(Runnable {
-            // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            // Preview
             val preview = Preview.Builder()
                     .build()
                     .also {
@@ -159,7 +214,7 @@ class MainActivity : AppCompatActivity() {
                     }
 
             imageCapture = ImageCapture.Builder()
-                .setFlashMode(FLASH_MODE_ON)
+                //.setFlashMode(FLASH_MODE_ON)
                     .build()
 
             val imageAnalyzer = ImageAnalysis.Builder()
@@ -187,7 +242,7 @@ class MainActivity : AppCompatActivity() {
                 camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalyzer)
 
                 if ( camera.getCameraInfo().hasFlashUnit() ) {
-                    camera.getCameraControl().enableTorch(true); // or false
+                    camera.getCameraControl().enableTorch(torchActive)
                 }
 
             } catch (exc: Exception) {
@@ -196,41 +251,32 @@ class MainActivity : AppCompatActivity() {
 
         }, ContextCompat.getMainExecutor(this))
 
-        val successSound = MediaPlayer.create(applicationContext, R.raw.sound_success)
-        val failureSound = MediaPlayer.create(applicationContext, R.raw.sound_failure)
-        val checkDistance = Runnable {
-            val text = findViewById<TextView>(R.id.color_name2)
 
 
-            while (true) {
-
-                if (currentState == States.SCAN) {
-                    if (!failureSound.isPlaying) {
-                        failureSound.start()
-                    }
-                    if (distance != null && distance!! < 10) {
-                        if (failureSound.isPlaying) {
-                            failureSound.stop()
-                        }
-                        if (!successSound.isPlaying) {
-                            successSound.start()
-                        }
-                        runOnUiThread({
-                            text.setText(currentColor)
-                        }
-                        )
-                        break
-                    }
-                }
-
-            }
-        }
-        Thread(checkDistance).start()
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
                 baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onPause() {
+        super.onPause()
+        threadsRunning = false;
+        failureSound.stop()
+        successSound.stop()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 
     override fun onDestroy() {
